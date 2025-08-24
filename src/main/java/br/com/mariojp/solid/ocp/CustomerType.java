d8faf2069cdfae4f12265128fe340c804fc2441a
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2025-08-24 17:44:06 -0300

    Criei essa branch para poder enviar outra solução que eu acho que está correta :]

--- src/main/java/br/com/mariojp/solid/ocp/CustomerType.java
+++ src/main/java/br/com/mariojp/solid/ocp/CustomerType.java
@@ -1,2 +1,13 @@
 package br.com.mariojp.solid.ocp;
-public enum CustomerType { REGULAR, PREMIUM, PARTNER }
+
+public enum CustomerType { 
+    REGULAR(0.95), 
+    PREMIUM(0.90), 
+    PARTNER(0.88);
+
+    public double value;
+
+    private CustomerType(double value) {
+        this.value = value;
+    }
+}
