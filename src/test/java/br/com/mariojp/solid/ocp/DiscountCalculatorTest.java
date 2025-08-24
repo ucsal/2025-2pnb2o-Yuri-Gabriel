9bd92509fc8841f066b6e3b5476eb70bce246d84
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2025-08-24 18:19:07 -0300

    Fiz essa versão da resolução mas criei outra branch, a 'alternativa', com uma segunda solução para o problema

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -3,10 +3,22 @@ package br.com.mariojp.solid.ocp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 public class DiscountCalculatorTest {
     @Test
     void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
+        HashMap<CustomerType, DiscountPolicy> map = new HashMap<CustomerType, DiscountPolicy>();
+        map.put(
+            CustomerType.PARTNER, new PartnerPolicy()
+        );
+        map.put(
+            CustomerType.PREMIUM, new PremiumPolicy()
+        );
+        map.put(
+            CustomerType.REGULAR, new RegularPolicy()
+        );
+        var calc = new DiscountCalculator(map);
         assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
             "PARTNER deveria ter 12% de desconto");
     }
